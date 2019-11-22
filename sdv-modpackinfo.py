7f2c1c3e90e8f49ba5699e4c005069167759babe
Bauke Conijn <bcmpinc@users.sourceforge.net>
2019-11-22 02:37:03 +0100

    Now uses better info from api.smapi.io

--- sdv-modpackinfo.py
+++ sdv-modpackinfo.py
@@ -18,69 +18,151 @@
 #
 
 import yaml
+import json
 import sys
 import os
 import re
+import urllib.request
 
 # To change the format, modify the lines below:
 format_url = "- [{Name}]({url}), v{Version} by **{Author}**: {Description}"
 format_nourl = "- {Name}, v{Version} by **{Author}**: {Description}"
 
-def default(info, key, value):
-    if not key in info:
-        info[key] = value
-
-def report(info):
-    default(info, "Author", "(unknown)")
-    default(info, "Description", "-")
-    if "MajorVersion" in info["Version"]:
-        version = info["Version"]
-        version = [
+# Scan the manifest.json files of the mods in the pack
+mods = []
+def scan(basedir):
+  res = False
+  for file in os.listdir(basedir):
+    path = os.path.join(basedir, file, "manifest.json")
+    if os.path.exists(path):
+      try:
+        with open(path) as f:
+          data = f.read()
+          data = data.replace("\t", " ")
+          data = re.sub("//.*?[\n]", "", data)
+          info = yaml.load(data, Loader=yaml.FullLoader)
+        
+        # Some data sanitization
+        if "MajorVersion" in info["Version"]:
+          version = info["Version"]
+          version = [
             version["MajorVersion"],
             version["MinorVersion"],
             version["PatchVersion"],
             version["Build"],
-        ]
-        info["Version"] = ".".join([str(i) for i in version if not i is ""])
-
-    try:
-        # It'll take the first updatekey
-        key = info["UpdateKeys"][0]
-    except:
-        print("Update key is missing.", file=sys.stderr)
-        print(format_nourl.format(**info))
-        return
-
-    try:
-        site, index = key.split(":")
-    except:
-        print("Update key is invalid: '"+key+"'.", file=sys.stderr)
-        print(format_nourl.format(**info))
-        return
-
-    if site == "Nexus":
-        url = "https://www.nexusmods.com/stardewvalley/mods/" + index
+          ]
+          info["Version"] = ".".join([str(i) for i in version if not i is ""])
+        
+        # Add to our modlist.
+        mods.append(info)
+        res = True
+      except err:
+        print("Error: Failed to parse " + path, file=sys.stderr)
+        print(err, file=sys.stderr)
     else:
-        print("Unrecognized updatekey: '"+key+"'.", file=sys.stderr)
-        print(format_nourl.format(**info))
-        return
-    
-    print("OK.", file=sys.stderr)
-    print(format_url.format(url = url, **info))
+      res2 = scan(os.path.join(basedir, file))
+      if res2:
+        res = True
+      else:
+        print("Warning: No mods found in " + path, file=sys.stderr)
+  
+  return res
 
-
-def scan(basedir):
-    for file in os.listdir(basedir):
-        print(file, file=sys.stderr, end=" : ")
-        path = os.path.join(basedir, file, "manifest.json")
-        if os.path.exists(path):
-            with open(path) as f:
-                data = f.read()
-                data = data.replace("\t", " ")
-                data = re.sub("//.*?[\n]", "", data)
-                json = yaml.load(data, Loader=yaml.FullLoader)
-            report(json)
-        else:
-            scan(os.path.join(basedir, file))
-
+print("Parsing manifest.json files", file=sys.stderr)
 scan("Mods")
+print("Sorting {0} mods".format(len(mods)), file=sys.stderr)
+mods.sort(key=lambda x: x["Name"])
+
+# Obtain mod info from smapi.io
+fake_unique_id = 0
+def mod_id(info):
+  res = {}
+  if "UniqueID" in info:
+    res["id"] = info["UniqueID"]
+  else:
+    print("ERROR: UniqueID is missing for " + info["Name"], file=sys.stderr)
+    res["id"] = info["UniqueID"] = "FAKE." + fake_unique_id
+    fake_unique_id += 1
+  if "UpdateKeys" in info:
+    keys = [x for x in info["UpdateKeys"] if ":" in x]
+    if keys:
+      res["updateKeys"] = keys
+  #res["installedVersion"] = info["Version"]
+  return res
+
+post_data = bytes(json.dumps({
+  "mods": [mod_id(x) for x in mods],
+  "includeExtendedMetadata": True
+}),"utf-8")
+
+#print(post_data)
+print("Requesting better info from api.smapi.io", file=sys.stderr)
+req = urllib.request.Request("https://api.smapi.io/v3.0/mods", method="POST", data=post_data)
+req.add_header("Content-Type", "application/json")
+res = urllib.request.urlopen(req)
+if res.status != 200:
+  print("Failed to obtain info from api.smapi.io: " + res.status + " " + res.reason, file=sys.stderr)
+  os.exit(1)
+print("Parsing response", file=sys.stderr)
+better_info = {x["id"]:x["metadata"] for x in json.loads(str(res.read(), "utf-8")) if "main" in x["metadata"]}
+
+# Output a formatted list of the mods contained in the pack.
+def default(info, key, value):
+  if not key in info:
+    info[key] = value
+
+def check_version(better, kind, version):
+    return kind in better and version == better[kind]["version"]
+
+stat_better = 0
+stat_guessed = 0
+def guess_url(info):
+  global stat_better, stat_guessed
+  if info["UniqueID"] in better_info:
+    # SMAPI had info about this mod, obtain the url from there
+    stat_better += 1
+    better = better_info[info["UniqueID"]]
+    version = info["Version"]
+    if check_version(better, "optional", version):
+      return better["optional"]["url"]
+    if check_version(better, "unofficial", version):
+      return better["unofficial"]["url"]
+    return better["main"]["url"]
+
+  # Otherwise, hope that the UpdateKeys have decent information.
+  try:
+    # It'll take the first updatekey
+    key = info["UpdateKeys"][0]
+  except:
+    print("Update key is missing for " + info["Name"], file=sys.stderr)
+    return None
+
+  try:
+    site, index = key.split(":")
+  except:
+    print("Update key is invalid for " + info["Name"] + ": '"+key+"'.", file=sys.stderr)
+    return None
+
+  if site == "Nexus":
+    stat_guessed += 1
+    return "https://www.nexusmods.com/stardewvalley/mods/" + index
+  else:
+    print("Unrecognized updatekey for " + info["Name"] + ": '"+key+"'.", file=sys.stderr)
+    return None  
+
+def report(info):
+  default(info, "Author", "(unknown)")
+  default(info, "Description", "-")
+
+  url = guess_url(info)
+  
+  if url:
+    print(format_url.format(url = url, **info))
+  else:
+    print(format_nourl.format(**info))
+
+print("Writing output", file=sys.stderr)
+for i in mods:
+  report(i)
+
+print("Done ({0} mods, with {1} guessed and {2} better urls).".format(len(mods), stat_guessed, stat_better), file=sys.stderr)
