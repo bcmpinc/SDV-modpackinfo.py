dd43f0eff968be69717d19705034ad050f38a3b8
Bauke Conijn <bcmpinc@users.sourceforge.net>
2019-11-24 19:59:26 +0100

    Make the script interactive

--- sdv-modpackinfo.py
+++ sdv-modpackinfo.py
@@ -23,25 +23,72 @@ import os
 import re
 import urllib.request
 
-# To change the format, modify the lines below:
+# These are the output formats supported by the script.
+# To add support for a new format, add a new entry.
+formats = [
+  dict(name="Markdown",
+       output_file="modlist.md",
+       format_url="- [{Name}]({url}), v{Version} by **{Author}**: {Description}",
+       format_nourl = "- {Name}, v{Version} by **{Author}**: {Description}"),
+]
 
-# Markdown format:
-output_file = "modlist.md"
-format_url = "- [{Name}]({url}), v{Version} by **{Author}**: {Description}"
-format_nourl = "- {Name}, v{Version} by **{Author}**: {Description}"
+# The directory that will be scanned for mods.
+mod_basedir = "Mods"
+
+# Request better mod info from smapi.io?
+request_smapi = False
 
 ####################################
+
+def die(ret=1):
+  print("Press enter to close.")
+  input()
+  sys.exit(ret)
+
+if not os.path.isdir(mod_basedir):
+  print("Error: '{0}' is not a directory".format(mod_basedir), file=sys.stderr)
+  print("Please run this from the directory where Stardew Valley is installed.".format(mod_basedir), file=sys.stderr)
+  die()
+
+# Select the output format
+def select_format():
+  global formats
+  print("Available output formats:")
+  for i in range(len(formats)):
+    print("  [{0}] {1}".format(i, formats[i]["name"]))
+  print("Please select the output format (by number or name): ", end="")
+  format_input = input().lower()
+
+  try:
+    chosen_format = next((i for i in formats if i["name"].lower() == format_input), None)
+    if not chosen_format:
+      chosen_format = formats[int(format_input)]
+  except Exception as err:
+    print("The entered value '{0}' is not a valid format: {1}".format(format_input, err), file=sys.stderr)
+    die()
+    
+  return chosen_format
+
+locals().update(select_format())
 
 # Scan the manifest.json files of the mods in the pack
 mods = []
 def scan(basedir):
+  """Scans the given directory for mods. Recurses into subdirectories if manifest.json is missing. Returns true if any mods were found."""
   res = False
   for file in os.listdir(basedir):
-    if file[0] == ".": continue
-    path = os.path.join(basedir, file, "manifest.json")
-    if os.path.exists(path):
+    # Skip Linux hidden files.
+    if file[0] == ".": continue 
+    
+    # Check if file is a directory, otherwise skip
+    path = os.path.join(basedir, file)
+    if not os.path.isdir(path):
+      print("Warning: Skipping '{0}' as it is not a directory.".format(path), file=sys.stderr)
+      
+    manifest = os.path.join(path, "manifest.json")
+    if os.path.exists(manifest):
       try:
-        with open(path, encoding="utf-8-sig") as f:
+        with open(manifest, encoding="utf-8-sig") as f:
           data = f.read()
           # data = data.replace("\t", " ")
           data = re.sub("//.*?[\n]", "", data)
@@ -62,11 +109,11 @@ def scan(basedir):
         # Add to our modlist.
         mods.append(info)
         res = True
-      except err:
-        print("Error: Failed to parse " + path, file=sys.stderr)
+      except Exception as err:
+        print("Error: Failed to parse " + manifest, file=sys.stderr)
         print(err, file=sys.stderr)
     else:
-      res2 = scan(os.path.join(basedir, file))
+      res2 = scan(path)
       if res2:
         res = True
       else:
@@ -74,8 +121,9 @@ def scan(basedir):
   
   return res
 
+print(file=sys.stderr)
 print("Parsing manifest.json files", file=sys.stderr)
-scan("Mods")
+scan(mod_basedir)
 print("Sorting {0} mods".format(len(mods)), file=sys.stderr)
 mods.sort(key=lambda x: x["Name"])
 
@@ -101,16 +149,23 @@ post_data = bytes(json.dumps({
   "includeExtendedMetadata": True
 }),"utf-8")
 
-#print(post_data)
-print("Requesting better info from api.smapi.io", file=sys.stderr)
-req = urllib.request.Request("https://api.smapi.io/v3.0/mods", method="POST", data=post_data)
-req.add_header("Content-Type", "application/json")
-res = urllib.request.urlopen(req)
-if res.status != 200:
-  print("Failed to obtain info from api.smapi.io: " + res.status + " " + res.reason, file=sys.stderr)
-  os.exit(1)
-print("Parsing response", file=sys.stderr)
-better_info = {x["id"]:x["metadata"] for x in json.loads(str(res.read(), "utf-8")) if "main" in x["metadata"]}
+# Info in manifest.json files is very unreliable unfortunately, so request better info from smapi.io.
+def get_better_info():
+  if not request_smapi: return {}
+  print(file=sys.stderr)
+  print("Requesting better info from api.smapi.io", file=sys.stderr)
+  req = urllib.request.Request("https://api.smapi.io/v3.0/mods", method="POST", data=post_data)
+  req.add_header("Content-Type", "application/json")
+  res = urllib.request.urlopen(req)
+  if res.status != 200:
+    print("Warning: Failed to obtain info from api.smapi.io: " + res.status + " " + res.reason, file=sys.stderr)
+    return {}
+  else:
+    print("Parsing response", file=sys.stderr)
+    res = {x["id"]:x["metadata"] for x in json.loads(str(res.read(), "utf-8")) if "main" in x["metadata"]}
+    return res
+  
+better_info = get_better_info()
 
 # Output a formatted list of the mods contained in the pack.
 def default(info, key, value):
@@ -167,10 +222,12 @@ def report(info, f_out):
   else:
     print(format_nourl.format(**info), file=f_out)
 
-print("Writing output", file=sys.stderr)
+print(file=sys.stderr)
+print("Writing {0} output to '{1}'".format(name, output_file), file=sys.stderr)
 with open(output_file, 'w') as f_out:
   for i in mods:
     report(i, f_out)
 
+print(file=sys.stderr)
 print("Done ({0} mods, with {1} guessed and {2} better urls).".format(len(mods), stat_guessed, stat_better), file=sys.stderr)
-
+die(0)
